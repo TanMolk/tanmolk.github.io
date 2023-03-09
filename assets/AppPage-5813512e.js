f6fbc1d0f17ac355f93e823bbe1b3becbbe0726e
TanMolk <114300129+TanMolk@users.noreply.github.com>
2023-03-09 21:31:24 +0000

    Delete assets directory

--- assets/AppPage-5813512e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as p}from"./index-e888ebbf.js";const t={name:"AppPage"};function a(c,n,r,s,_,l){return o(),p("p",null,"Hello App")}const m=e(t,[["render",a]]);export{m as default};
