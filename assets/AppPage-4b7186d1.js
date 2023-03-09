400734b369bf1474119f9e1c3cb905cecca62ef0
TanMolk <114300129+TanMolk@users.noreply.github.com>
2023-03-10 05:31:43 +0800

    Add files via upload

---
+++ assets/AppPage-4b7186d1.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as p}from"./index-68d04e30.js";const t={name:"AppPage"};function a(c,n,r,s,_,l){return o(),p("p",null,"Hello App")}const m=e(t,[["render",a]]);export{m as default};
